78841ba6623f0cd1c9a55abac951c10768ac2f61
Yoru-cyber <carlosmendez170210@gmail.com>
2024-05-18 23:33:36 -0400

    initial commit

---
+++ app/main.py
@@ -0,0 +1,134 @@
+from flask import Flask, request
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.orm import DeclarativeBase
+from sqlalchemy import Integer, String
+from sqlalchemy.orm import Mapped, mapped_column
+from flasgger import Swagger
+
+class Base(DeclarativeBase):
+    pass
+
+
+db = SQLAlchemy(model_class=Base)
+
+app = Flask(__name__)
+app.config['SWAGGER'] = {
+    'title': 'User service API',
+    'uiversion': 2
+}
+swagger = Swagger(app)
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///project.db"
+
+db.init_app(app)
+
+
+class User(db.Model):
+    id: Mapped[int] = mapped_column(primary_key=True)
+    username: Mapped[str] = mapped_column(unique=True)
+    email: Mapped[str]
+    password: Mapped[str]
+
+
+with app.app_context():
+    db.create_all()
+
+
+@app.route("/users", methods=["POST"])
+def create_user():
+    """Creates a new user in the database.
+    ---
+    parameters:
+        - name: username
+          in: body
+          required: true
+          type: string
+        - name: password
+          required: true
+          in: body
+          type: string
+        - name: email
+          required: true
+          in: body
+          type: string
+    definitions:
+        User:
+            type: object
+            properties:
+                username: 
+                    type: string
+                password: 
+                    type: string
+                email: 
+                    type: string
+    responses:
+        201:
+            description: confirmation message
+            content: application/json
+            schema: 
+                $ref: '#/definitions/User'
+            examples:
+                message: 'user created'
+    """
+    user = User(
+        username=request.form["username"],
+        email=request.form["email"],
+        password=request.form["password"],
+    )
+    db.session.add(user)
+    db.session.commit()
+    return {"message": "user created"}, 201
+
+
+@app.route("/users/<int:id>", methods=["GET"])
+def user_detail(id):
+    """Retrieves an user from database
+    ---
+    parameters:
+        - name: id
+          in: path
+          type: integer
+          required: true
+    responses:
+        201:
+            description: confirmation message
+            content: application/json
+            schema: 
+                $ref: '#/definitions/User'
+            examples:
+                username: 'foo'
+                password: 'bar'
+                email: 'foobar@something.com'
+    """
+    user = db.get_or_404(User, id)
+    return {
+        "Username": user.username,
+        "Email": user.email,
+        "Password": user.password,
+    }, 200
+
+
+@app.route("/user/<int:id>", methods=["POST"])
+def user_delete(id):
+    """Retrieves an user from database
+    ---
+    parameters:
+        - name: id
+          in: path
+          type: integer
+          required: true
+    responses:
+        201:
+            description: confirmation message
+            content: application/json
+            schema: 
+                $ref: '#/definitions/User'
+            examples:
+                message: 'user deleted'
+    """
+    user = db.get_or_404(User, id)
+    db.session.delete(user)
+    db.session.commit()
+    return {"message": "user deleted"}, 200
+
+
+
